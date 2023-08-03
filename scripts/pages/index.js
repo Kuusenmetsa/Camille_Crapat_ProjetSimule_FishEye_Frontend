a392752360849fd5256c756e6828187f8ef21bc6
Camille Crapat <camille.capt@gmail.com>
2023-08-03 12:12:31 +0200

    ajout de commentaire et séparation de l'élèment img

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -13,7 +13,7 @@ async function displayData(photographers) {
 	const photographersSection = document.querySelector('.photographer_section');
 
 	photographers.forEach((photographer) => {
-		const photographerModel = photographerTemplate(photographer);
+		const photographerModel = cardPhotographerTemplate(photographer);
 		const userCardDOM = photographerModel.getUserCardDOM();
 		photographersSection.appendChild(userCardDOM);
 	});
